909367111b931bea4fe109607fc6b537e08bbd50
Kevin Nguy <72185570+KevinPNguy01@users.noreply.github.com>
2024-10-28 14:14:39 -0700

    Implemented color interpolation

--- Software Rasterizer.cpp
+++ Software Rasterizer.cpp
@@ -43,17 +43,35 @@ COLORREF RGBtoBGR(COLORREF color) {
     return RGB(blue, green, red);
 }
 
-void DrawTriangle(void* bits, const Vec3& v0, const Vec3& v1, const Vec3& v2, COLORREF color) {
+Vec3 computeBarycentric(const Vec3& v0, const Vec3& v1, const Vec3& v2, const Vec3& p) {
+    float denominator = (v1[1] - v2[1]) * (v0[0] - v2[0]) + (v2[0] - v1[0]) * (v0[1] - v2[1]);
+    float alpha = ((v1[1] - v2[1]) * (p[0] - v2[0]) + (v2[0] - v1[0]) * (p[1] - v2[1])) / denominator;
+    float beta = ((v2[1] - v0[1]) * (p[0] - v2[0]) + (v0[0] - v2[0]) * (p[1] - v2[1])) / denominator;
+    float gamma = 1.0f - alpha - beta;
+    return Vec3(alpha, gamma, beta);
+}
+
+void DrawTriangle(void* bits, const Vec3& v0, const Vec3& v1, const Vec3& v2, COLORREF c0, COLORREF c1, COLORREF c2) {
     int width = ceil(screenWidth * (max(v0[0], max(v1[0], v2[0])) - min(v0[0], min(v1[0], v2[0]))));
     int height = ceil(screenHeight * (max(v0[1], max(v1[1], v2[1])) - min(v0[1], min(v1[1], v2[1]))));
 
     for (int x = 0; x < width; ++x) {
         for (int y = 0; y < height; ++y) {
-            if (InsideTriangle(v0, v1, v2, Vec3(((float)x + 0.5) / width, ((float)y + 0.5) / height, 0))) {
-                ((DWORD*)bits)[y * screenWidth + x] = RGBtoBGR(color);
+            Vec3 p = Vec3(((float)x + 0.5) / width, ((float)y + 0.5) / height, 0);
+            if (InsideTriangle(v0, v1, v2, p)) {
+                Vec3 b = computeBarycentric(v0, v1, v2, p);
+                COLORREF c = RGB(
+                    b[0] * GetRValue(c0) + b[1] * GetRValue(c1) + b[2] * GetRValue(c2),
+                    b[0] * GetGValue(c0) + b[1] * GetGValue(c1) + b[2] * GetGValue(c2),
+                    b[0] * GetBValue(c0) + b[1] * GetBValue(c1) + b[2] * GetBValue(c2)
+                ); 
+                ((DWORD*)bits)[y * screenWidth + x] = RGBtoBGR(c);
             }
         }
     }
+}
+void DrawTriangle(void* bits, const Vec3& v0, const Vec3& v1, const Vec3& v2, COLORREF c) {
+    DrawTriangle(bits, v0, v1, v2, c, c, c);
 }
 
 void DrawTriangle(void* bits, const Vec3& v0, const Vec3& v1, const Vec3& v2) {
@@ -80,7 +98,7 @@ int main()
     HDC hdcMem = CreateCompatibleDC(hdcDest);
     SelectObject(hdcMem, hBitmap);
 
-    DrawTriangle(bits, Vec3(0, 0, 0), Vec3(1, 0.5, 0), Vec3(0.5, 1, 0), RGB(0, 0, 255));
+    DrawTriangle(bits, Vec3(0, 0, 0), Vec3(1, 0.5, 0), Vec3(0.5, 1, 0), RGB(255, 0, 0), RGB(0, 255, 0), RGB(0, 0, 255));
     while (1) {
         RenderFrame(hwnd, hdcMem);
     }
